26d9604d9b6238da2b20ff60e301a9318f66e73e
Braxton McClellan <thebraxmc@gmail.com>
2025-03-05 22:24:00 -0700

    added ability to delete and update items

--- src/App.js
+++ src/App.js
@@ -12,11 +12,27 @@ function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="App">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItems}
+        onToggleItem={handleToggleItem}
+      />
       <Stats />
     </div>
   );
--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,11 +1,11 @@
 import Item from "./Item";
 
-export default function PackingList({ items }) {
+export default function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
         ))}
       </ul>
     </div>
